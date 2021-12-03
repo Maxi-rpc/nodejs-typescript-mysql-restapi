01b0804e1a1d75c23c335beddc5dcf29d2dd5f5a
Maximiliano <maxirpc2607@gmail.com>
2021-12-03 16:22:14 -0300

    Add morgan mysql
    Create script database

---
+++ sql/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE nodejs_typscipt_mysql_restapi;
+
+CREATE TABLE posts(
+    id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(200) NOT NULL,
+    description TEXT NOT NULL,
+    image_url TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DESCRIBE posts;
